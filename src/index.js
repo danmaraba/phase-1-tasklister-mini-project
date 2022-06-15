3e2b849b8cf94c63638a7a1a5cd9d8c124e52ee1
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-15 05:06:40 +0300

    Done with this assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector("#create-task-form").addEventListener("submit", (e)=>{
+    e.preventDefault()
+    buildToDo(e.target.new_task_description.value)
+  })
+  })
+
+  function buildToDo(todo) {
+    let p=document.createElement('p')
+    let btn=document.createElement('button')
+    btn.addEventListener('click',handleDelete)
+    btn.textContent= 'x'
+    p.textContent= `${todo}`
+    p.appendChild(btn)
+    console.log(p)
+    document.querySelector('#tasks').appendChild(p)
+  }
+  
+  function handleDelete(e) {
+    e.target.parentNode.remove()
+    
+  }
+
